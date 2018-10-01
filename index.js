311c138aa1eb18697dbb31c5d30200a9e1c31923
Destin Moulton <destin.moulton@gmail.com>
2018-10-01 11:56:39 -0600

    Added logic for silent execution and emailing.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const minimist = require("minimist");
 
 const CLI = require("./lib/cli");
+const email = require("./lib/email");
+const fileio = require("./lib/fileio");
+const siteup = require("./lib/siteup");
 
 const argv = minimist(process.argv.slice(2));
 
 if (argv["silent"] !== undefined && argv["silent"] === true) {
     // Run the silent version
+    try {
+        (async () => {
+            const sites = await siteup.checkMultiple(fileio.getSites());
+            email.send(sites);
+        })();
+    } catch (err) {
+        console.error(err);
+    }
 } else {
     cli = new CLI();
     cli.getSitesStatus();
