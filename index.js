d2452311d46b26a572e0ef40a444b9ca123bcd66
Destin Moulton <destin.moulton@gmail.com>
2018-10-01 11:40:06 -0600

    Added  silent argument option.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const Prompt = require("./lib/prompt");
+const minimist = require("minimist");
 
-prompt = new Prompt();
-prompt.inputSelectSite();
+const CLI = require("./lib/cli");
+
+const argv = minimist(process.argv.slice(2));
+
+if (argv["silent"] !== undefined && argv["silent"] === true) {
+    // Run the silent version
+} else {
+    cli = new CLI();
+    cli.getSitesStatus();
+}
